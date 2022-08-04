830f0b295b34791668a864e38f52f9458fa41d07
Nathalia Silva <Silva.Nathalia@ibm.com>
2022-08-04 16:06:37 -0300

    desafio

---
+++ Desafio-Data-04082022/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio_Data_04082022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+            double n = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = n * (raio * raio);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
